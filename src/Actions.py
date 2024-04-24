605f98fde69700b8a25e8d5dbddc1c3ff249e93f
busra <busra.cagliyan@pardus.org.tr>
2024-04-24 16:26:58 +0300

    add writing conf files with config parser and fix some syntax

--- src/Actions.py
+++ src/Actions.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import configparser
 import os
 import sys
 import subprocess
@@ -18,6 +19,7 @@ locale.bindtextdomain(APPNAME, TRANSLATIONS_PATH)
 locale.textdomain(APPNAME)
 locale.setlocale(locale.LC_ALL, SYSTEM_LANGUAGE)
 
+
 def main():
     def control_lock():
         apt_pkg.init_system()
@@ -27,65 +29,95 @@ def main():
             return False
         apt_pkg.pkgsystem_unlock()
         return True
-    
+
     def update():
-        subprocess.call(["apt", "update", "-o", "APT::Status-Fd=2"],
-                        env={**os.environ, 'DEBIAN_FRONTEND': 'noninteractive'})
+        subprocess.call(
+            ["apt", "update", "-o", "APT::Status-Fd=2"],
+            env={**os.environ, "DEBIAN_FRONTEND": "noninteractive"},
+        )
 
     def install(package_list):
-        for package in  package_list:
-            subprocess.call(["apt", "install", package, "-yq", "-o", "APT::Status-Fd=2"],
-                        env={**os.environ, 'DEBIAN_FRONTEND': 'noninteractive'})
+        for package in package_list:
+            subprocess.call(
+                ["apt", "install", package, "-yq", "-o", "APT::Status-Fd=2"],
+                env={**os.environ, "DEBIAN_FRONTEND": "noninteractive"},
+            )
 
-    def update_hostname_file(comp_name,domain):
-        # to check file /etc/hostname 
+    def update_hostname_file(comp_name, domain):
+        # to check file /etc/hostname
         hostname_file = "/etc/hostname"
-        with open(hostname_file, 'r') as file:
+        with open(hostname_file, "r") as file:
             current_hostname = file.readline().strip()
             print(_("checking /etc/hostname file..."))
-            if comp_name+"."+domain not in current_hostname:
+            if comp_name + "." + domain not in current_hostname:
                 print(_("added domain name to /etc/hostname file"))
-                with open(hostname_file, 'w') as file:
-                    new_hostname = "{}.{}".format(comp_name,domain)
+                with open(hostname_file, "w") as file:
+                    new_hostname = "{}.{}".format(comp_name, domain)
                     file.write(new_hostname)
             else:
                 print(_("done"))
 
-    def update_hosts_file(comp_name,domain):
-        # to check file /etc/hosts 
+    def update_hosts_file(comp_name, domain):
+        # to check file /etc/hosts
         hosts_file = "/etc/hosts"
-        with open(hosts_file, 'r') as file:
+        with open(hosts_file, "r") as file:
             lines = file.readlines()
         print(_("checking /etc/hosts file..."))
         new_hosts_file = []
-        domain_exists = False 
+        domain_exists = False
 
         for line in lines:
             if line.strip().startswith("127.0.1.1"):
                 if f"{comp_name}.{domain}" not in line:
                     line = f"127.0.1.1 {comp_name}.{domain} {comp_name}\n"
-                domain_exists = True 
+                domain_exists = True
             new_hosts_file.append(line)
 
         if not domain_exists:
             new_hosts_file.append(f"127.0.1.1 {comp_name}.{domain} {comp_name}\n")
 
-        with open(hosts_file, 'w') as file:
+        with open(hosts_file, "w") as file:
             file.writelines(new_hosts_file)
 
         if domain_exists:
             print(_("done"))
         else:
             print(_("added domain name to /etc/hosts file"))
-            
-    def join(comp_name,domain,user,passwd,ouaddress,smb_settings):
+
+    def rewrite_conf(file, settings):
+        config = configparser.RawConfigParser()
+        config.optionxform = str  # This prevents it from converting keys to lowercase
+
+        if os.path.exists(file):
+            config.read(file)
+
+        for section, options in settings.items():
+            if not config.has_section(section):
+                config.add_section(section)
+            for key, value in options.items():
+                config.set(section, key, value)
+
+        with open(file, "w") as configfile:
+            config.write(configfile)
+
+    def join(comp_name, domain, user, passwd, ouaddress, smb_settings):
         try:
             # to update
             update()
 
             # to install packages
-            package_list = ["krb5-user", "samba", "sssd", "libsss-sudo", 
-                       "realmd", "packagekit", "adcli", "sssd-tools", "cifs-utils", "smbclient"]
+            package_list = [
+                "krb5-user",
+                "samba",
+                "sssd",
+                "libsss-sudo",
+                "realmd",
+                "packagekit",
+                "adcli",
+                "sssd-tools",
+                "cifs-utils",
+                "smbclient",
+            ]
             install(package_list)
 
             # to join domain
@@ -95,13 +127,13 @@ def main():
 
             update_hostname_file(comp_name, domain)
             update_hosts_file(comp_name, domain)
-            
+
             try:
                 result = subprocess.check_output(["realm", "discover"]).decode("utf-8")
 
                 for line in result.split("\n"):
                     if line.strip().startswith("domain-name:"):
-                        if line.split(":")[1] == " "+domain:
+                        if line.split(":")[1] == " " + domain:
                             print(_("joining the domain..."))
                         else:
                             print(_("Domain name check: False"))
@@ -112,101 +144,87 @@ def main():
                 print(_("Not reachable, check your DNS address."), file=sys.stdout)
                 sys.exit(1)
 
-            command = "realm join -v --computer-ou=\""+ouaddress+"\" --user=\""+user+"@"+domain.upper()+"\" "+domain.lower()
-            process = subprocess.Popen([command],stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-            process.communicate(passwd.encode('utf-8'))
+            command = (
+                'realm join -v --computer-ou="'
+                + ouaddress
+                + '" --user="'
+                + user
+                + "@"
+                + domain.upper()
+                + '" '
+                + domain.lower()
+            )
+            process = subprocess.Popen(
+                [command], stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True
+            )
+            process.communicate(passwd.encode("utf-8"))
 
             password_check = process.returncode
             if password_check == 1:
                 print(_("Domain username or password check: False"), file=sys.stdout)
-            
+
             if smb_settings == "True":
-                # Samba Authentication            
+                # Samba Authentication
                 # rewrite file /etc/samba/smb.conf
                 smb_file = "/etc/samba/smb.conf"
-                with open(smb_file, "w") as file:
-                    file.write(
-                        """[global]
-    unix charset = UTF-8
-    workgroup = {}
-    client signing = yes
-    client use spnego = yes
-    dedicated keytab file = /etc/krb5.keytab
-    kerberos method = secrets and keytab
-    realm = {}
-    dns proxy = no
-    map to guest = Bad User
-    log file = /var/log/samba/log.%m
-    max log size = 1000
-    syslog = 0
-""".format(domain.split(".")[0].upper(),domain))
+                samba_settings = {
+                    "global": {
+                        "unix charset": "UTF-8",
+                        "workgroup": domain.split(".")[0].upper(),
+                        "client signing": "yes",
+                        "client use spnego": "yes",
+                        "dedicated keytab file": "/etc/krb5.keytab",
+                        "kerberos method": "secrets and keytab",
+                        "realm": domain,
+                        "dns proxy": "no",
+                        "map to guest": "Bad User",
+                        "log file": "/var/log/samba/log.%m",
+                        "max log size": 1000,
+                        "syslog": 0,
+                    },
+                }
+                rewrite_conf(smb_file, samba_settings)
 
             # to check and rewrite file /etc/sssd/sssd.conf
             sssd_file = "/etc/sssd/sssd.conf"
-            settings = {
-                "use_fully_qualified_names": "False",
-                "ad_gpo_access_control":"permissive",
-                "ad_gpo_ignore_unreadable":"True"
+            sssd_settings = {
+                "sssd": {
+                    "domains": domain,
+                    "config_file_version": 2,
+                    "services": "nss, pam",
+                },
+                f"domain/{domain}": {
+                    "default_shell": "/bin/bash",
+                    "ad_server": domain,
+                    "krb5_store_password_if_offline": True,
+                    "cache_credentials": True,
+                    "krb5_realm": domain,
+                    "realmd_tags": "manages-system joined-with-adcli",
+                    "id_provider": "ad",
+                    "fallback_homedir": "/home/%u@%d",
+                    "ad_domain": domain.upper(),
+                    "use_fully_qualified_names": False,
+                    "ldap_id_mapping": True,
+                    "access_provider": "ad",
+                    "ad_gpo_access_control": "permissive",
+                    "ad_gpo_ignore_unreadable": True,
+                },
             }
-            if os.path.exists(sssd_file):
-                new_sssd_conf = []
-                with open(sssd_file,"r") as sfile:
-                    contents = sfile.readlines()
-
-                    for line in contents:
-                        for key, value in settings.items():
-                            if line.strip().startswith(f"{key}"):
-                                if value not in line:
-                                    new_sssd_conf.append(f"{key} = {value}\n")
-                                break
-                        else:
-                            new_sssd_conf.append(line)
-                # to add missing variables
-                for key,value in settings.items():
-                    if f"{key} = {value}\n" not in new_sssd_conf:
-                        new_sssd_conf.append(f"{key} = {value}\n")
-
-                with open(sssd_file,"w") as sfile:
-                    for item in new_sssd_conf:
-                        sfile.write(item)
-            else:
-                with open(sssd_file,"w") as sfile:
-                    sfile.write(
-                        """
-[sssd]
-domains = {}
-config_file_version = 2
-services = nss, pam
-
-[domain/{}]
-default_shell = /bin/bash
-ad_server = {}
-krb5_store_password_if_offline = True
-cache_credentials = True
-krb5_realm ={}
-realmd_tags = manages-system joined-with-adcli 
-id_provider = ad
-fallback_homedir = /home/%u@%d
-ad_domain ={}
-use_fully_qualified_names = False
-ldap_id_mapping = True
-access_provider = ad
-ad_gpo_access_control = permissive
-ad_gpo_ignore_unreadable = True
-""".format(domain,domain,domain,domain.upper(),domain)
-                    )
-            subprocess.call(["chmod", "600", sssd_file])   
-            subprocess.call(["pam-auth-update", "--enable ", "pardus-pam-config"],
-                            env={**os.environ, 'DEBIAN_FRONTEND': 'noninteractive'}) 
+            rewrite_conf(sssd_file, sssd_settings)
+            subprocess.call(["chmod", "600", sssd_file])
+            subprocess.call(
+                ["pam-auth-update", "--enable ", "pardus-pam-config"],
+                env={**os.environ, "DEBIAN_FRONTEND": "noninteractive"},
+            )
             subprocess.call(["systemctl", "restart ", "sssd"])
 
-            if password_check==0:
+            if password_check == 0:
                 print(_("This computer has been successfully added to the domain."))
         except Exception as e:
             print(e)
 
     def permit():
-        subprocess.call(["realm","permit","-a"])
+        subprocess.call(["realm", "permit", "-a"])
 
     def leave():
         subprocess.call(["realm", "leave", "-v"])
@@ -214,11 +232,18 @@ ad_gpo_ignore_unreadable = True
     if len(sys.argv) > 1:
         if control_lock():
             if sys.argv[1] == "join":
-                join(sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6],sys.argv[7])
+                join(
+                    sys.argv[2],
+                    sys.argv[3],
+                    sys.argv[4],
+                    sys.argv[5],
+                    sys.argv[6],
+                    sys.argv[7],
+                )
                 permit()
             elif sys.argv[1] == "host":
-                update_hostname_file(sys.argv[2],sys.argv[3])
-                update_hosts_file(sys.argv[2],sys.argv[3])
+                update_hostname_file(sys.argv[2], sys.argv[3])
+                update_hosts_file(sys.argv[2], sys.argv[3])
             elif sys.argv[1] == "leave":
                 leave()
         else:
@@ -226,6 +251,7 @@ ad_gpo_ignore_unreadable = True
             sys.exit(1)
     else:
         print("no argument passed")
+
 
 if __name__ == "__main__":
     main()
